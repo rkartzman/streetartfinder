00a4f0d37dd208316cdbabec9e12cb0c7de5f5d6
ivanyukio <ivan.yk14@gmail.com>
2015-03-06 21:44:53 -0500

    add js code to allow camera work

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
-console.log(Geolocation.latLng());
-console.log(Geolocation.currentLocation());
 navigator.geolocation.getCurrentPosition(function (position) {
   var lat = position.coords.latitude;
   var longi = position.coords.longitude;
   console.log(lat, longi);
 });
+
+if(Meteor.isClient){
+  Template.takePhoto.events({
+    'click .capture': function(){
+      MeteorCamera.getPicture({}, function(error, data){
+  Session.set('photo', data);
+});
+    }
+  });
+}
