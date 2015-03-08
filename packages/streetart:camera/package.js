4366b3123d9ce188a161b8dc86a916f3b8dca4c7
ivanyukio <ivan.yk14@gmail.com>
2015-03-08 11:32:50 -0400

    add caption to photo

---
+++ packages/streetart:camera/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: "streetart:camera",
+  summary: "Photos with one function call on desktop and mobile.",
+  version: "1.1.4",
+  git: "https://github.com/meteor/mobile-packages"
+});
+
+Cordova.depends({
+  "org.apache.cordova.camera":"0.3.2"
+});
+
+Package.onUse(function(api) {
+  api.export('MeteorCamera');
+  api.use(["templating", "session", "ui", "blaze", "less", "reactive-var"]);
+  api.versionsFrom("METEOR@0.9.2");
+
+  api.addFiles('photo.html');
+  api.addFiles('photo.js');
+  api.addFiles("camera.less", ["web.browser"]);
+  api.addFiles('photo-browser.js', ['web.browser']);
+  api.addFiles('photo-cordova.js', ['web.cordova']);
+});
