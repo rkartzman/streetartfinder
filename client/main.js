19250b2234de76f378471023bccab03e6bcce737
ivanyukio <ivan.yk14@gmail.com>
2015-03-08 11:32:50 -0400

    add caption to photo

--- client/main.js
+++ client/main.js
@@ -1,53 +1,45 @@
-// currentPosition = (function(){
-
-getCoordinates = function(){
-		return Geolocation.latLng();
-};
-
-
-
-	// if currentPosition
-	// and meteor is client
-	// return {
-	// 	lat: lat,
-	// 	lng: longi
-	// }
-// })();
+  getCoordinates = function(){
+    return Geolocation.latLng();
+  };
 
 function dataURItoBlob(dataURI) {
-    var binary = atob(dataURI.split(',')[1]);
-    var array = [];
-    for(var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i));
-    }
-    return new Uint8Array(array);
+  var binary = atob(dataURI.split(',')[1]);
+  var array = [];
+  for(var i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i));
+  }
+  return new Uint8Array(array);
 
     //return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
-}
+  }
 
+// $(document).on("keypress", "#caption", function(){picCaption = $('#caption').val();})
 
 
 if(Meteor.isClient){
   Template.takePhoto.events({
     'click .capture': function(){
       MeteorCamera.getPicture({}, function(error, data){
-      Session.set('photo', data);
-      var blob = dataURItoBlob(data);
-      var newOne = Photos.insert({});
-      var fileName = newOne + ".jpg";
-      Meteor.call("upload_to_s3", fileName, blob);
-      Photos.update(
-        {_id: newOne},
-        {
-        $set: {
-          url: "http://s3.amazonaws.com/streetartfinder/" + fileName,
-          coordinates: {lat: Geolocation.latLng().lat,
-          lng: Geolocation.latLng().lng}
-        }
-      });
+        Session.set('photo', data);
+        var blob = dataURItoBlob(data);
+        var newOne = Photos.insert({});
+        var fileName = newOne + ".jpg";
+        Meteor.call("upload_to_s3", fileName, blob);
+        Photos.update(
+          {_id: newOne},
+          {
+            $set: {
+              url: "http://s3.amazonaws.com/streetartfinder/" + fileName,
+              coordinates: {lat: Geolocation.latLng().lat,
+                lng: Geolocation.latLng().lng,
+        // caption: picCaption
+      }
+    }
+  });
 
-	});
+      });
     }
   });
 }
+
 
