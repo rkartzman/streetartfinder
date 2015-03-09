b9bdc440367f4039f1c7ede2245f2e23eee15abd
David Deneroff <dgdeneroff@gmail.com>
2015-03-09 11:10:01 -0400

    Comments working

---
+++ client/template/photos/comments.js
@@ -0,0 +1,20 @@
+Template.comments.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var commentValue = event.target.comment.value;
+      var photoId = event.target.photoId.value;
+      Photos.update({_id: photoId}, {
+        $addToSet: {
+          comments: { content: commentValue,
+                user: Meteor.user() }
+              }
+        })
+      event.target.comment.value = ""
+    }
+});
+
+Template.comments.helpers({
+  photoComments: function() {
+    return this.comments
+  }
+});
