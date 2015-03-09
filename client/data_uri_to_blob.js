eb82972d48900653c93667242e7abd0f337f958d
Lucas Santos <lucasrsantos1@gmail.com>
2015-03-09 13:45:13 -0400

    Move function to proper file

---
+++ client/data_uri_to_blob.js
@@ -0,0 +1,9 @@
+function dataURItoBlob(dataURI) {
+  var i;
+  var binary = atob(dataURI.split(',')[1]);
+  var array = [];
+  for (i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i));
+  }
+  return new Uint8Array(array);
+}
