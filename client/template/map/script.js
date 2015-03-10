61fbc270ae2f15deec24f83036a09cbe15e21cac
David Deneroff <dgdeneroff@gmail.com>
2015-03-10 16:28:10 -0400

    Add loop for different pins

--- client/template/map/script.js
+++ client/template/map/script.js
@@ -14,14 +14,20 @@ Template.showMap.rendered = function () {
     map = new google.maps.Map(element, options);
 
     marks = [];
-    var addMarker = function (lat, lng, photoId) {
+    var addMarker = function (lat, lng, photoId, seen) {
+      if($.inArray( Meteor.user()._id, seen )) {
+        var icon = 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-265cb2/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/waterfilter.png'
+      } else {
+        var icon = 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-c259b5/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/snowman.png'
+      };
       mark = (new google.maps.Marker({
         id: photoId,
         position: {
           lat: lat,
           lng: lng
         },
-        map: map
+        map: map,
+        icon: icon
       }));
 
       if ($.inArray(mark, marks) === -1) {
@@ -34,7 +40,7 @@ Template.showMap.rendered = function () {
     google.maps.event.addListenerOnce(map, 'tilesloaded', function (evt) {
       var i;
       for (i = 0; i < Photos.find().count(); i++) {
-        mapster.addMarker(parseFloat(Photos.find().fetch()[i].coordinates.lat), parseFloat(Photos.find().fetch()[i].coordinates.lng), Photos.find().fetch()[i]._id);
+        mapster.addMarker(parseFloat(Photos.find().fetch()[i].coordinates.lat), parseFloat(Photos.find().fetch()[i].coordinates.lng), Photos.find().fetch()[i]._id, Photos.find().fetch()[i].seen);
       }
       var mapClusterer = new MarkerClusterer(map, marks, mcOptions);
       marks.forEach(function (mark) {
