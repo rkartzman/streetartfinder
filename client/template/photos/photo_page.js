95ae783a2e773e7be6bbb0b67c3a1de07c5bec06
David Deneroff <dgdeneroff@gmail.com>
2015-03-10 16:29:01 -0400

    Add photoPage template helper

--- client/template/photos/photo_page.js
+++ client/template/photos/photo_page.js
@@ -3,3 +3,16 @@ Template.photoPage.helpers({
     return this.usersVotedUp.length;
   }
 });
+
+Template.photoPage.events({
+    'click #been-there': function(event){
+      event.preventDefault();
+      console.log(event.target);
+      var photoId = this._id;
+      Photos.update({_id: photoId}, {
+        $addToSet: {
+          seen: Meteor.user()._id
+              }
+        })
+    }
+});
