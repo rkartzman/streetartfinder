a376d1c6871f774271e9a784979eaa2a4ac58df4
Lucas Santos <lucasrsantos1@gmail.com>
2015-03-09 20:46:55 -0400

    Fix unlike button code

--- client/template/photos/downvote.js
+++ client/template/photos/downvote.js
@@ -1,23 +1,15 @@
 Template.downvoteForm.events({
-  'submit form': function(event){
+  'submit form': function (event) {
     event.preventDefault();
     var photoId = event.target.photoId.value;
     var userId = [Meteor.user()._id];
     var photo = Photos.find({_id: photoId}).fetch()[0];
-    if($.inArray(userId[0], photo.usersVotedDown) === -1 ) {
+    if ($.inArray(userId[0], photo.usersVotedUp) !== -1) {
       Photos.update({_id: photoId}, {
-        $addToSet: { usersVotedDown: userId[0] },
-        $inc: { downvotes: 1 }
-      })
+        $pull: {
+          usersVotedUp: userId[0]
+        }
+      });
     }
-    // if($.inArray(userId[0], photo.usersVotedUp !== -1)) {
-    //   Photos.update({_id: photoId}, {
-    //     $inc: { downvotes: 2 }
-    //   })
-    //     photo.usersVotedUp.splice($.inArray(userId[0]), 1)
-    //     photo.usersVotedDown.splice($.inArray(userId[0]), 1)
-    // }
-    // console.log(photo.usersVotedDown)
-    // console.log(photo.usersVotedUp)
   }
 });
