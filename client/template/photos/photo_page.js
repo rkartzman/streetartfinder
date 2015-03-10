6ab927ad0a9159acf8abe0897b4922ee6147bb21
rkartzman <remykartzman@gmail.com>
2015-03-10 19:13:35 -0400

    Add helpers for returning the lat and longitude

--- client/template/photos/photo_page.js
+++ client/template/photos/photo_page.js
@@ -1,5 +1,11 @@
 Template.photoPage.helpers({
   getVotes: function () {
     return this.usersVotedUp.length;
+  }, 
+  getLat: function() {
+  	return this.coordinates.lat; 
+  }, 
+  getLng: function(){
+  	return this.coordinates.lng;
   }
 });
