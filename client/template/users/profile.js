0f73c10c971965827e19d3b41fc93d31532187a6
David Deneroff <dgdeneroff@gmail.com>
2015-03-10 22:37:58 -0400

    Unstyled photo page

--- client/template/users/profile.js
+++ client/template/users/profile.js
@@ -24,3 +24,42 @@ Template.userProfile.helpers({
     return Meteor.user().emails[0].address
   }
 })
+
+Template.userProfile.events({
+    'click #user-photos-check':function(event){
+      event.preventDefault();
+      $('.profile-photos').toggle(true);
+      $('.been-there-user').toggle(false);
+      $('.manage-account').toggle(false);
+    }
+});
+
+Template.userProfile.helpers({
+  beenTherePhotos: function(){
+    return Photos.find({seen: Meteor.user()._id})
+  }
+})
+
+Template.userProfile.events({
+    'click #user-been-there':function(event){
+      event.preventDefault();
+      $('.been-there-user').toggle(true);
+      $('.profile-photos').toggle(false);
+      $('.manage-account').toggle(false);
+    }
+});
+
+Template.userProfile.events({
+    'click #user-manage-account':function(event){
+      event.preventDefault();
+      $('.manage-account').toggle(true);
+      $('.been-there-user').toggle(false);
+      $('.profile-photos').toggle(false);
+    }
+});
+
+Template.userProfile.events({
+ 'click #delete-photo-user':function(){
+    Photos.remove({_id: this._id}, {justOne: true});
+  }
+})
